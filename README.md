e599c138cbd299f13e15b3a78003732548918f0f
Ignacio <icolodron@stratio.com>
2020-01-10 13:29:05 +0100

    first version that run OK
    first version that run OK

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # seller-client
 generic app for put in contact sellers that offer articles or services and clients
+
+Usé esta página para comenzar el proyecto: https://medium.com/@ashton.s.fernandes/the-mern-stack-with-redux-fbd2a1400fa
+
+### Installing
+
+To install the dependencies:
+
+```
+npm install
+```
+
+To run in development:
+```
+npm run dev
+```
+To run the server:
+```
+npm run server
+```
+To run the client:
+```
+npm run client
+```
+You have to edit the MongoDB URI in keys.js with your MongoDB URI
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/client/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: '/',
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HashedModuleIdsPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Boilerplate",
+      template: "index.ejs",
+      minify: {
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true
+      }
+    })
+  ],
+  mode: "production"
+};
